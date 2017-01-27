c1a23c9c17383d48f5ec39fab209ddc04d0d258c
zdarzeniowka <zdarzeniowka@gmail.com>
2017-01-28 00:54:23 +0100

    refaktoryzacja tabeli Artur

---
+++ src/main/resources/db/V2_CREATE_VIEW_SCORERS_CLASSIFICATION.sql
@@ -0,0 +1,17 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `watking_prime`@`%` 
+    SQL SECURITY DEFINER
+VIEW `SCORERS_CLASSIFICATION` AS
+    SELECT 
+        `p`.`NAME` AS `Zawodnik`,
+        `t`.`NAME` AS `Druzyna`,
+        COUNT(`me`.`ID`) AS `Bramki`
+    FROM
+        ((`MATCH_EVENT` `me`
+        JOIN `PLAYER` `p` ON ((`me`.`PLAYER_ID` = `p`.`ID`)))
+        JOIN `TEAM` `t` ON ((`p`.`TEAM_ID` = `t`.`ID`)))
+    WHERE
+        (`me`.`EVENT_TYPE_ID` = 1)
+    GROUP BY 1 , 2
+    ORDER BY `Bramki` DESC
